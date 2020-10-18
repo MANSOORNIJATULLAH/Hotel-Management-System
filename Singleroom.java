4e3b7039e6df32097087825eac88ac05e5583668
nijatullahmansoor <55934107+nijatullahmansoor@users.noreply.github.com>
2020-10-18 16:02:42 +0530

    The sigleroom.java class is implemented

---
+++ Singleroom.java
@@ -0,0 +1,27 @@
+import java.io.Serializable;
+
+public class Singleroom implements Serializable{
+
+	String name;
+	String contact;
+	String gender;
+
+	ArrayList<Food> food = new ArrayList<>();   //Lab1 : implementaion of array in java
+
+	public Singleroom(){
+
+		this.name="";
+
+	}
+
+	public Singleroom(String name, String contact, String gender){
+
+		// Lab6 : Implementaion of this keyword 
+		this.name = name;
+		this.contact = contact;
+		this.gender = gender;
+	}
+
+	
+
+}
